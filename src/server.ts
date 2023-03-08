17c6fdf5d2830bf16ffda51e9dd08ec41c532ce5
Jess Mark Sevilleja <jess@clinica.dev>
2023-03-08 16:58:33 +0800

    added initial database entities and jwt

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,49 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import jwt from "jsonwebtoken";
+import { connectToDatabase } from "./database";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors());
+// Connect to database
+connectToDatabase()
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database:", err);
+  });
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+// Middleware
+//app.use(morgan("dev")); for logs
+//app.use(helmet()); for security
+app.use(cors());
+app.use(express.json());
+
+// Authentication middleware
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET || "secret",
+    (err: any, user: any) => {
+      if (err) return res.sendStatus(403);
+      req.body.user = user;
+      next();
+    }
+  );
+}
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Robot Tracker API!");
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
