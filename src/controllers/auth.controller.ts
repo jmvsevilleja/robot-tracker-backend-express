dc7d0a7da09db22d99cbc9c12f3eba60c1365135
Jess Mark Sevilleja <jess@clinica.dev>
2023-03-09 20:49:31 +0800

    updated auth route to controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { getRepository } from "typeorm";
+import { User } from "../entities/user.entity";
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // Check if email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  // Find the user by email
+  const userRepository = getRepository(User);
+  const user = await userRepository.findOne({ where: { email: email } });
+
+  // Check if the user exists
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  // Check if the password is correct
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  // Generate and return a JWT token
+  const token = jwt.sign(
+    { userId: user.id, email: user.email },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "1h" }
+  );
+  res.json({ token });
+};
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,41 +1,9 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { getRepository } from "typeorm";
-import { User } from "../entities/user.entity";
+import { loginUser } from "../controllers/auth.controller";
 
 const router = Router();
 
 // Login route
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  // Check if email and password are provided
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and password are required" });
-  }
-
-  // Find the user by email
-  const userRepository = getRepository(User);
-  const user = await userRepository.findOne({ where: { email: email } });
-
-  // Check if the user exists
-  if (!user) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  // Check if the password is correct
-  const isPasswordCorrect = await bcrypt.compare(password, user.password);
-  if (!isPasswordCorrect) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  // Generate and return a JWT token
-  const token = jwt.sign(
-    { userId: user.id },
-    process.env.JWT_SECRET || "default_secret"
-  );
-  res.json({ token });
-});
+router.post("/login", loginUser);
 
 export default router;
