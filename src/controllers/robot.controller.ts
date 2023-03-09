a51003950663f1bd489a0a8a6b3de28092b45cee
Jess Mark Sevilleja <jess@clinica.dev>
2023-03-09 19:11:07 +0800

    added get update delete route controller

--- src/controllers/robot.controller.ts
+++ src/controllers/robot.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { Robot } from "../entities/robot.entity";
 import { getAvatarUrl } from "../utils/avatar.util";
+const ObjectId = require("mongodb").ObjectId;
 
 export const createRobot = async (req: Request, res: Response) => {
   const { name, purpose } = req.body;
@@ -25,4 +26,51 @@ export const getRobots = async (req: Request, res: Response) => {
 
   const robots = await robotRepository.find();
   res.json(robots);
+};
+
+export const getRobot = async (req: Request, res: Response) => {
+  const robotRepository = getRepository(Robot);
+  const id = new ObjectId(req.params.id);
+
+  const robot = await robotRepository.findOne(id);
+  if (!robot) {
+    return res.status(404).json({ message: "Robot not found" });
+  }
+  res.json(robot);
+};
+
+export const updateRobot = async (req: Request, res: Response) => {
+  const robotRepository = getRepository(Robot);
+  const id = new ObjectId(req.params.id);
+
+  const robot = await robotRepository.findOne(id);
+  if (!robot) {
+    return res.status(404).json({ message: "Robot not found" });
+  }
+
+  const { name, purpose } = req.body;
+  robot.name = name ?? robot.name;
+  robot.purpose = purpose ?? robot.purpose;
+  robot.avatarUrl = await getAvatarUrl(name);
+
+  await robotRepository.save(robot);
+  res.json(robot);
+};
+
+export const deleteRobot = async (req: Request, res: Response) => {
+  const robotRepository = getRepository(Robot);
+  const id = new ObjectId(req.params.id);
+
+  const robot = await robotRepository.findOne(id);
+  if (!robot) {
+    return res.status(404).json({ message: "Robot not found" });
+  }
+
+  await robotRepository.delete(robot);
+  res
+    .json({
+      status: "success",
+      message: `Successfully deleted robot with id ${id}`,
+    })
+    .sendStatus(204);
 };
--- src/routes/robots.route.ts
+++ src/routes/robots.route.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { getRobots, createRobot } from "../controllers/robot.controller";
+import {
+  getRobots,
+  getRobot,
+  createRobot,
+  updateRobot,
+  deleteRobot,
+} from "../controllers/robot.controller";
 
 const router = express.Router();
 
 router.get("/", getRobots);
 router.post("/", createRobot);
+router.get("/:id", getRobot);
+router.put("/:id", updateRobot);
+router.delete("/:id", deleteRobot);
 
 export default router;
